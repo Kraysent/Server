5cd58ff6c036143c5af0fb1cded605b03e764bee
Artyom Zaporozhets <kraysent@gmail.com>
2022-06-26 19:42:00 +0300

    add test for mappers

--- go.mod
+++ go.mod
@@ -7,10 +7,12 @@ require (
 	github.com/heetch/confita v0.10.0
 	github.com/jackc/pgx/v4 v4.16.1
 	github.com/rs/zerolog v1.15.0
+	github.com/stretchr/testify v1.7.0
 )
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.12.1 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -21,7 +23,9 @@ require (
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ pkg/core/storage/mappers_test.go
@@ -0,0 +1,63 @@
+package storage
+
+import (
+	"server/pkg/core/entities"
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type MapperTestSuite struct {
+	suite.Suite
+	storage *Storage
+}
+
+func (s *MapperTestSuite) SetupTest() {
+	config := StorageConfig{
+		DSN: "host=localhost port=5432 user=testserver password=passw0rd dbname=testserverdb sslmode=disable",
+	}
+	zerolog.SetGlobalLevel(zerolog.Disabled)
+
+	s.storage = NewStorage(config)
+	s.storage.Connect()
+
+	_, _ = s.storage.Create(entities.User{
+		Login:        "test1",
+		Salt:         1,
+		PasswordHash: "weak_hash",
+		Description:  "weak desc",
+	})
+	_, _ = s.storage.Create(entities.User{
+		Login:        "test2",
+		Salt:         2,
+		PasswordHash: "middle_hash",
+		Description:  "middle desc",
+	})
+	_, _ = s.storage.Create(entities.User{
+		Login:        "test3",
+		Salt:         3,
+		PasswordHash: "strong_hash",
+		Description:  "strong_desc",
+	})
+}
+
+func (s *MapperTestSuite) TestGet() {
+	actual, _ := s.storage.Get("test2")
+	assert.Equal(s.T(), entities.User{
+		ID:           2,
+		Login:        "test2",
+		Salt:         2,
+		PasswordHash: "middle_hash",
+		Description:  "middle desc",
+	}, *actual)
+}
+
+func (s *MapperTestSuite) TearDownSuite() {
+	s.storage.Disconnect()
+}
+
+func TestMapperTestSuite(t *testing.T) {
+	suite.Run(t, new(MapperTestSuite))
+}
